1525e3c8f036bf2d370b149d79af31680c6f0c2a
AbdulMueedd <144735650+AbdulMueedd@users.noreply.github.com>
2025-12-24 01:29:24 -0600

    Add files via upload

---
+++ src/main/java/com/pl/primerleague/PrimerLeagueApplication.java
@@ -0,0 +1,13 @@
+package com.pl.primerleague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerLeagueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrimerLeagueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pl/primerleague/PrimerLeagueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pl.primerleague;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimerLeagueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
